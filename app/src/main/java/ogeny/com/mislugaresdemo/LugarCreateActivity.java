5f34bc3a1cfe9a00d7ac8f96c22ee51b39535781
Oscar Saavedra <saavedraog@outlook.com>
2022-07-07 17:45:14 -0500

    menu principal

---
+++ app/src/main/java/ogeny/com/mislugaresdemo/LugarCreateActivity.java
@@ -0,0 +1,14 @@
+package ogeny.com.mislugaresdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LugarCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lugar_create);
+    }
+}
