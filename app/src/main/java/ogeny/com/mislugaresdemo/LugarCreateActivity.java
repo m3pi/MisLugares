4006ddde373a0e9fae0fe077ab7f57e34929f617
Oscar Saavedra <saavedraog@outlook.com>
2022-07-07 20:39:18 -0500

    Uso práctico de Vistas y Layouts

--- app/src/main/java/ogeny/com/mislugaresdemo/LugarCreateActivity.java
+++ app/src/main/java/ogeny/com/mislugaresdemo/LugarCreateActivity.java
@@ -3,12 +3,36 @@ package ogeny.com.mislugaresdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LugarCreateActivity extends AppCompatActivity {
+    private EditText entrada;
+    private TextView salida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lugar_create);
+
+        entrada = (EditText) findViewById(R.id.edt_entrada);
+        salida = (TextView) findViewById(R.id.tv_salida);
+    }
+
+    public void pulsar(View view) {
+        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
+    }
+
+    public void pulsarCero(View view) {
+        /*entrada.setText(entrada.getText() + "0");*/
+//        entrada.setText(entrada.getText() + (String) view.getTag());
+
+
+        salida.setText(String.valueOf(Float.parseFloat(
+                entrada.getText().toString()) * 2.0));
+
+
     }
 }
