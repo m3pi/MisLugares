240623b483bcc36049cfb37e5778c066e24f887a
Oscar Saavedra <saavedraog@outlook.com>
2022-07-07 19:50:29 -0500

    Estilos y temas

--- app/src/main/java/ogeny/com/mislugaresdemo/ScrollingActivity.java
+++ app/src/main/java/ogeny/com/mislugaresdemo/ScrollingActivity.java
@@ -1,5 +1,7 @@
 package ogeny.com.mislugaresdemo;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -60,5 +62,21 @@ public class ScrollingActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // Para acceder a un recurso definido en los ejemplos anteriores desde Java usaremos el siguiente código:
+    private void GetRecursoXML() {
+        Resources res = getResources();
+        Drawable drawable =  res.getDrawable(R.drawable.ic_launcher_background);
+        String titulo = res.getString(R.string.app_name);
+
+        /*int color =res.getColor(R.color.verde_opaco);
+        float tamanoFuente =res.getDimension(R.dimen.tamano_fuente);
+        int maxAsteroides =res.getInteger(R.integer.max_asteroides);
+        boolean ilimitados = res.getBoolean(R.bool.misiles_ilimitados);
+        String[] diasSemana =res.getStringArray(R.array.dias_semana);
+        int[] primos =res.getIntArray(R.array.primos);
+        TypedArray asteroides =res.obtainTypedArray(R.array.asteroides);
+        Drawable asteroide1 =asteroides.getDrawable(0);*/
     }
 }
