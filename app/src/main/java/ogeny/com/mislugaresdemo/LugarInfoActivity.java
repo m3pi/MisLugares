aea121aa391064755e4f8529ad29483e88ce955a
Oscar Saavedra <saavedraog@outlook.com>
2022-07-27 00:50:36 -0500

    Añadiendo fotografías. - (avance) Añadir fotografía desde la cámara

--- app/src/main/java/ogeny/com/mislugaresdemo/LugarInfoActivity.java
+++ app/src/main/java/ogeny/com/mislugaresdemo/LugarInfoActivity.java
@@ -6,13 +6,24 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
@@ -23,6 +34,9 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -34,10 +48,15 @@ public class LugarInfoActivity extends AppCompatActivity {
     // con ActivityResultLauncher no es necesario los códigos
     // final static int RESULTADO_EDITAR = 1;
 
-    // añadiendo imágenes
+    // añadiendo imágenes desde la galeria
     private ImageView fotoLugar;
     final static int RESULTADO_GALERIA = 2;
     final static int RESULTADO_FOTO = 3;
+
+    // añadiendo imágenes desde la cámara
+    private Uri uriFotoLugar;
+//    private static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".provider";
+    private static final String AUTHORITY = BuildConfig.APPLICATION_ID;
 
     ActivityResultLauncher<Intent> activityResultLauncher =
             registerForActivityResult(
@@ -64,6 +83,24 @@ public class LugarInfoActivity extends AppCompatActivity {
                         insertarFotoLugar(fotoLugar, lugar.getFoto());
                     } else {
                         Toast.makeText(LugarInfoActivity.this, "Foto no cargada", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+    ActivityResultLauncher<Intent> camaraArl = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Integer a = result.getResultCode();
+
+                    Log.i("resultado", a.toString());
+                    if (result.getResultCode() == Activity.RESULT_OK
+                    && lugar != null
+                    && uriFotoLugar != null) {
+                        lugar.setFoto(uriFotoLugar.toString());
+                        insertarFotoLugar(fotoLugar, lugar.getFoto());
+                    } else {
+                        Toast.makeText(LugarInfoActivity.this, "Error en captura", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
@@ -237,9 +274,97 @@ public class LugarInfoActivity extends AppCompatActivity {
 
     protected void insertarFotoLugar(ImageView imageView, String uri) {
         if (uri != null && !uri.isEmpty() && !uri.equals("null")) {
-            imageView.setImageURI(Uri.parse(uri));
+            if (uri.startsWith("content://ogeny.com.mislugaresdemo/") ||
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.
+                            READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                imageView.setImageBitmap(reduceBitmap(this, uri, 1024,   1024));
+            } else {
+                imageView.setImageURI(Uri.parse(uri));
+            }
+
         } else {
             imageView.setImageBitmap(null);
+        }
+    }
+
+    public static Bitmap reduceBitmap(Context contexto, String uri,
+                                      int maxAncho, int maxAlto) {
+        try {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(contexto.getContentResolver()
+                    .openInputStream(Uri.parse(uri)), null, options);
+            options.inSampleSize = (int) Math.max(
+                    Math.ceil(options.outWidth / maxAncho),
+                    Math.ceil(options.outHeight / maxAlto));
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeStream(contexto.getContentResolver()
+                    .openInputStream(Uri.parse(uri)), null, options);
+        } catch (FileNotFoundException e) {
+            Toast.makeText(contexto, "Fichero/recurso no encontrado",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void tomarFoto(View view) {
+        /*Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");*/
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        /*uriFotoLugar = Uri.fromFile(
+                new File(Environment.getExternalStorageDirectory() + File.separator
+                + "img_" + (System.currentTimeMillis() / 1000) + ".jpg")
+        );*/
+/*
+        File file = new File(Environment.getExternalStorageDirectory() + File.separator
+                + "img_" + (System.currentTimeMillis() / 1000) + ".jpg");
+        uriFotoLugar = FileProvider.getUriForFile(this, AUTHORITY, file);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoLugar);
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+        else if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN) {
+            ClipData clip=
+                    ClipData.newUri(getContentResolver(), "A photo", uriFotoLugar);
+
+            intent.setClipData(clip);
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+        else {
+//            List<ResolveInfo> resInfoList=
+//                    getPackageManager()
+//                            .queryIntentActivities(i, PackageManager.MATCH_DEFAULT_ONLY);
+//
+//            for (ResolveInfo resolveInfo : resInfoList) {
+//                String packageName = resolveInfo.activityInfo.packageName;
+//                grantUriPermission(packageName, outputUri,
+//                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+//            }
+        }*/
+
+        File file = null;
+        try {
+            File rutaImagenes = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            file = File.createTempFile("img_" + (System.currentTimeMillis() / 1000),
+                    ".jpg",
+                    rutaImagenes
+            );
+        } catch (IOException e) {
+//            e.printStackTrace();
+            Toast.makeText(this, "Error al crear imagen temporal", Toast.LENGTH_SHORT).show();
+        }
+
+        try {
+            if (file != null) {
+                uriFotoLugar = FileProvider.getUriForFile(this, AUTHORITY, file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoLugar);
+                camaraArl.launch(intent);
+            }
+        }
+        catch (ActivityNotFoundException e) {
+            Log.e("mompi", e.toString());
+            Toast.makeText(this, "R.string.msg_no_camera", Toast.LENGTH_LONG).show();
+            finish();
         }
     }
 
